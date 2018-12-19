381fdb8f7d84a7b7af7efee74abb303cc4f0cb63
AaronStahley <acstahle@asu.edu>
2018-12-19 10:40:34 -0700

    finished array cardio pt2

---
+++ src/07 - Array Cardio Day 2/index.js
@@ -0,0 +1,80 @@
+// ## Array Cardio Day 2
+
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+
+// const isAdult = people.some(function (person) {
+//     const currentYear = (new Date()).getFullYear();
+//     if(currentYear - person.year >= 19) {
+//         return true;
+//     }
+// })
+
+/**
+ * Simpler way to write it compared to above.
+ */
+const isAdult = people.some(person =>
+    ((new Date()).getFullYear()) - person.year >= 19 );
+
+
+console.log({isAdult});
+
+// Array.prototype.every() // is everyone 19 or older?
+const allAdults = people.every(person =>
+    ((new Date()).getFullYear()) - person.year >= 19 );
+
+console.log({allAdults});
+
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+
+// const comment = comments.find(function(comment) {
+//     if(comment.id === 823423) {
+//         return true;
+//     }
+// })
+
+/**
+ * Simpler way than above using arrow and implicit return.
+ */
+const comment = comments.find(comment => comment.id === 823423);
+
+console.log(comment);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const inex = comments.findIndex(comment => comment.id ===
+    823423);
+
+/**
+ * This is the simple way to delete it from the array.
+ */
+//coments.splice(index, 1);
+
+/**
+ * This creates a new array with the comment not in it
+ * this is popular when using redux.
+ */
+const newComments = [
+    ...comments.slice(0, index),
+    ...comment.slice(index + 1)
+];
